eec6aafcbc5bfc4f0a38c290c04714da0008a26a
blunoob <farhan.blu@gmail.com>
2016-03-21 12:48:04 +0500

    GameManager Starting point

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Game Manager is the main entry point to a game.
+/// </summary>
+public class GameManager : MonoBehaviour 
+{
+	public GameManager _instance	{	get;	private set;}
+
+	protected void Awake()
+	{
+		if(_instance == null) {
+			_instance = this;
+			DontDestroyOnLoad(gameObject);
+		} else
+			Destroy(gameObject);
+	}
+
+	protected void Start()
+	{
+
+	}
+}
